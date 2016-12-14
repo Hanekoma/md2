fabbcbb5dde14b17235d6d5a4f3ba00f7b123697
Sergi Tortosa <sergi.tortosa@est.fib.upc.edu>
2016-12-14 19:39:32 +0100

    inicio svm

--- SVM.py
+++ SVM.py
@@ -1,9 +1,10 @@
-import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-#
-#   **** Loading data
-#
+import scipy as sp
+from sklearn import cross_validation
+from sklearn import metrics
+from sklearn.cross_validation import cross_val_score
+from sklearn import svm
 
 url = "http://mlr.cs.umass.edu/ml/machine-learning-databases/iris/iris.data"
 
@@ -15,10 +16,6 @@ X = np.array(dataset[['Sepal Length', 'Sepal Width', 'Petal Length', 'Petal Widt
 y = np.array(dataset['Class'])
 
 
-#
-#   **** Some printing
-#
-
 from sklearn import preprocessing
 import matplotlib.pyplot as plt
 
@@ -27,10 +24,6 @@ le.fit(y)
 plt.scatter(X[:, 0], X[:, 1], c=le.transform(y), cmap=plt.cm.Paired)
 plt.show()
 
-from sklearn import cross_validation
-from sklearn.tree import DecisionTreeClassifier
-from sklearn import metrics
-from os import system
 
 #
 #   **** Simple cross-validation
@@ -40,42 +33,44 @@ X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_
 print(X_train.shape)
 print(X_test.shape)
 
-#from sklearn.neighbors import KNeighborsClassifier
-#clf = KNeighborsClassifier(n_neighbors=3).fit(X_train, y_train)
-#from sklearn.naive_bayes import GaussianNB
-#clf = GaussianNB().fit(X_train, y_train)
-from sklearn.tree import DecisionTreeClassifier
-clf = DecisionTreeClassifier(random_state=0).fit(X_train, y_train)
-clf.score(X_test, y_test)
-y_pred = clf.predict(X_test)
+
+#
+#   **** Aplicamos SVM ****
+#
+
+# SVM linear svm.SVC(C, kernel)
+linear_svc = svm.SVC(C=10, kernel='linear').fit(X_train, y_train)
+linear_svc.fit(X_train, y_train)
+
+# SVM polynomial svm.SVC(C, kernel, degree)
+poly_2 = svm.SVC(C=10, kernel='poly', degree=2).fit(X_train, y_train)
+
+poly_3 = svm.SVC(C=10, kernel='poly', degree=3).fit(X_train, y_train)
+
+
+# SVM rbf svm.SVC(C, kernel, gamma)
+rbf = svm.SVC(C=10, kernel="rbf", gamma=0.1).fit(X_train, y_train)
+
+# Predict
+linear_svc.score(X_test, y_test)
+y_pred = linear_svc.predict(X_test)
+
 print "Classification Report:"
 print metrics.classification_report(y_test, y_pred)
 print "Confusion Matrix:"
 print metrics.confusion_matrix(y_test, y_pred)
-
-# Only for Decision Trees - Print the tree 
-# Graphviz should be installed 
-from sklearn import tree
-dotfile = open("a.dot","w")
-tree.export_graphviz(clf, out_file = dotfile, feature_names = headers)
-dotfile.close()
-system("dot -Tpng a.dot -o a.png")
 
 
 #
 #   **** k-folds cross-validation
 #
 
-from sklearn.cross_validation import cross_val_score
-
 #clf = KNeighborsClassifier(n_neighbors=3)
 #clf = GaussianNB()
-clf = DecisionTreeClassifier(random_state=0)
+clf = svm.SVC(C=10, kernel="linear")
 print cross_val_score(clf, X, y, cv=10)
 print np.mean(cross_val_score(clf, X, y, cv=10))
 
 y_pred = cross_validation.cross_val_predict(clf, X, y, cv=10)
 print metrics.classification_report(y, y_pred)
 print metrics.confusion_matrix(y, y_pred)
-
-
