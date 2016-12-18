66fc340fa5d09bcee89bcbd49bae4e31e37ce6c5
Sigr3s <tortosa95@gmail.com>
2016-12-18 22:49:13 +0100

    oriol

--- SVMIrisData.py
+++ SVMIrisData.py
@@ -20,48 +20,22 @@ def normalizeAttribute(attr1):
     return (a_norm.tolist())[0]
 
 
-def transformGetData(df):  # esta funcion debe devolver los datos en un np.array()
-    # First Normalize numerical
-    AAGE = normalizeAttribute(df['AAGE'])
-    WKSWORKYEAR = normalizeAttribute(df['WKSWORKYEAR'])
-    CHILDS = normalizeAttribute(df['CHILDS'])
-    # MARSUPWT = normalizeAttribute(df['MARSUPWT']) NO FERLA SERVIR
-    # then categorical
-    F_ACLSWKR = np.array(pd.get_dummies(df['F_ACLSWKR'])).tolist()
-    F_EDUCATION = np.array(pd.get_dummies(df['F_EDUCATION'])).tolist()
-    F_STATUSMARIT = np.array(pd.get_dummies(df['F_STATUSMARIT'])).tolist()
-    F_AMJIND = np.array(pd.get_dummies(df['F_AMJIND'])).tolist()
-    F_AMJOCC = np.array(pd.get_dummies(df['F_AMJOCC'])).tolist()
-    F_RACE = np.array(pd.get_dummies(df['F_RACE'])).tolist()
-    F_ORIGIN = np.array(pd.get_dummies(df['F_ORIGIN'])).tolist()
-    F_ASEX = np.array(pd.get_dummies(df['F_ASEX'])).tolist()
-    F_AWKSTAT = np.array(pd.get_dummies(df['F_AWKSTAT'])).tolist()
-    F_FILESTATUS = np.array(pd.get_dummies(df['F_FILESTATUS'])).tolist()
-    F_HHDFMX = np.array(pd.get_dummies(df['F_HHDFMX'])).tolist()
-    F_HHDREL = np.array(pd.get_dummies(df['F_HHDREL'])).tolist()
-    F_CONBIRTHFATH = np.array(pd.get_dummies(df['F_CONBIRTHFATH'])).tolist()
-    F_CONBIRTHMOTH = np.array(pd.get_dummies(df['F_CONBIRTHMOTH'])).tolist()
-    F_PENATVTY = np.array(pd.get_dummies(df['F_PENATVTY'])).tolist()
-    F_PRCITSHP = np.array(pd.get_dummies(df['F_PRCITSHP'])).tolist()
-    res = []
-    for i in range(len(AAGE)):
-        res.append(
-            F_ACLSWKR[i] + F_EDUCATION[i] + F_STATUSMARIT[i] + F_AMJIND[i] + F_AMJOCC[i] + F_RACE[i] + F_ORIGIN[i] +
-            F_ASEX[i] + F_AWKSTAT[i] + F_FILESTATUS[i] + F_HHDFMX[i] + F_HHDREL[i] + F_CONBIRTHFATH[i] + F_CONBIRTHMOTH[
-                i] + F_PENATVTY[i] + F_PRCITSHP[i] + [AAGE[i], WKSWORKYEAR[i], CHILDS[i]])
-    return res
-
 
 def mainfunction():
     # Loading the Digits dataset
-    url = "C:\Users\sergi\Desktop\WorkSpace\md2\Census.csv"
+    url = "S:\UNI\WorkSpace\md2\Census.csv"
     df = pd.read_csv(url)
-    X = np.array(transformGetData(df))
-    y = np.array(df['TARGET'])
+    cleaned = pd.get_dummies(df,
+                             columns=['F_ACLSWKR', 'F_EDUCATION', 'F_STATUSMARIT', 'F_AMJIND', 'F_AMJOCC', 'F_RACE',
+                                      'F_ORIGIN', 'F_ASEX', 'F_AWKSTAT', 'F_FILESTATUS', 'F_HHDFMX', 'F_HHDREL',
+                                      'F_CONBIRTHFATH', 'F_CONBIRTHMOTH', 'F_PENATVTY', 'F_PRCITSHP'])
 
-    # Split the dataset in two equal parts
+    cols_to_norm = ['AAGE', 'WKSWORKYEAR', 'CHILDS']
+    cleaned[cols_to_norm] = cleaned[cols_to_norm].apply(lambda x: (x - x.mean()) / (x.max() - x.min()))
+    cleaned = cleaned.drop('MARSUPWT',1)
+    cleaned_wo_target = cleaned.drop('TARGET', 1)
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.5, random_state=0)
+        cleaned_wo_target, cleaned['TARGET'], test_size=0.25)
 
     # Set the parameters by cross-validation
     tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4],
@@ -99,48 +73,6 @@ def mainfunction():
         y_true, y_pred = y_test, clf.predict(X_test)
         print(classification_report(y_true, y_pred))
         print()
-
-        pca = PCA().fit(X_train)
-
-        PcaX = pca.transform(X_train)
-
-        # Generate grid along first two principal components
-        multiples = np.arange(-2, 2, 0.1)
-        # steps along first component
-        for j in range(5):
-            for m in range(5):
-                first = multiples[:, np.newaxis] * pca.components_[j, :]
-                # steps along second component
-                second = multiples[:, np.newaxis] * pca.components_[m, :]
-                # combine
-                grid = first[np.newaxis, :, :] + second[:, np.newaxis, :]
-                flat_grid = grid.reshape(-1, X.shape[1])
-
-                # title for the plots
-                titles = ['SVC with rbf kernel',
-                          'SVC (linear kernel)\n with Fourier rbf feature map\n'
-                          'n_components=100',
-                          'SVC (linear kernel)\n with Nystroem rbf feature map\n'
-                          'n_components=100']
-
-                #plt.tight_layout()
-                plt.figure(figsize=(12, 5))
-                plt.subplot(1, 3, 1)
-                sv = clf.best_estimator_.fit(X_train, y_train)
-                Z = sv.predict(flat_grid)
-                Z2 = np.array(pd.get_dummies(Z)).astype(float)
-                # Put the result into a color plot
-                Z = Z2.reshape(grid.shape[:-1])
-                plt.contourf(multiples, multiples, Z, cmap=plt.cm.Paired)
-                plt.axis('off')
-                colors = np.array(pd.get_dummies(y_train)).astype(float)
-                le = LabelEncoder()
-                col2 = le.fit(y_train).transform(y_train)
-                # Plot also the training points
-                plt.scatter(PcaX[:, j], PcaX[:, m], c=col2, cmap=plt.cm.Paired)
-                plt.title("PCA:" + str(j) + "and: "+ str(m))
-                plt.show()
-
 
 
 
