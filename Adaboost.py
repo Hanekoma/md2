6bc26d88af543d834dad1e3cf373d2b7ec3c6ff0
oriol.munoz <oriol.munoz@agilogy.com>
2016-12-19 18:24:56 +0100

    Adabost with different n_estimators

--- Adaboost.py
+++ Adaboost.py
@@ -1,10 +1,9 @@
-import pandas as pd
-import pickle
 import os
+import pickle
 
+import pandas as pd
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
 
 csv = pd.read_csv('../train.csv')
 # turn categoricals to new column with 0 or 1 (except TARGET)
@@ -14,39 +13,40 @@ train = pd.get_dummies(csv, columns=['F_ACLSWKR', 'F_EDUCATION', 'F_STATUSMARIT'
 train_wo_target = train.drop('TARGET', 1)
 
 
-def adaboost(estimator):
+def adaboost(estimator, n_estimators):
     estimator_name = type(estimator).__name__
     print("Adaboosting with estimator {}".format(estimator_name))
-    classifier = AdaBoostClassifier(base_estimator=estimator, n_estimators=200)
+    classifier = AdaBoostClassifier(base_estimator=estimator, n_estimators=n_estimators)
     target = train['TARGET']
 
     classifier.fit(train_wo_target, target)
-    with open('../{}'.format(estimator_name), 'wb') as file:
-        print("Storing model of {} estimator".format(estimator_name))
+    with open('../{}{}'.format(estimator_name, n_estimators), 'wb') as file:
+        print("Storing model of {} estimator with n_estimators={}".format(estimator_name, n))
         pickle.dump(obj=classifier, file=file)
     return classifier
 
 
 if __name__ == '__main__':
-    # check if we have already obtained the model previously
-    if not (os.path.isfile('../NoneType') and os.path.isfile('../DecisionTreeClassifier')):
-        ds = adaboost(None)  # decision stumps
-        dt = adaboost(DecisionTreeClassifier())  # decision trees
-    else:
-        with open('../NoneType', 'rb') as file:
-            ds = pickle.load(file)
-        with open('../DecisionTreeClassifier', 'rb') as file:
-            dt = pickle.load(file)
-    test_csv = pd.read_csv('../test.csv')
-    test_wo_label = test_csv.drop('TARGET', 1)
-    test = pd.get_dummies(test_wo_label)
-    missing_columns = [item for item in list(train_wo_target.keys()) if item not in list(test.keys())]
-    print("test dataset was missing {}; adding them with values 0".format(missing_columns))
-    for mc in missing_columns:
-        test[mc] = pd.Series(0, index=test.index)
-    dss = ds.score(test, test_csv['TARGET'])
-    print(
-        "The Adaboost method using decision stumps scores a precision of {}".format(dss))
-    dts = dt.score(test, test_csv['TARGET'])
-    print(
-        "The Adaboost method using decision trees scores a precision of {}".format(dts))
+    for n in [1, 2, 5, 10, 50, 100, 200]:
+        # check if we have already obtained the model previously
+        if not (os.path.isfile('../NoneType{}'.format(n)) and os.path.isfile('../DecisionTreeClassifier{}'.format(n))):
+            ds = adaboost(None, n)  # decision stumps
+            dt = adaboost(DecisionTreeClassifier(), n)  # decision trees
+        else:
+            with open('../NoneType{}'.format(n), 'rb') as file:
+                ds = pickle.load(file)
+            with open('../DecisionTreeClassifier{}'.format(n), 'rb') as file:
+                dt = pickle.load(file)
+        test_csv = pd.read_csv('../test.csv')
+        test_wo_label = test_csv.drop('TARGET', 1)
+        test = pd.get_dummies(test_wo_label)
+        missing_columns = [item for item in list(train_wo_target.keys()) if item not in list(test.keys())]
+        print("test dataset was missing {}; adding them with values 0".format(missing_columns))
+        for mc in missing_columns:
+            test[mc] = pd.Series(0, index=test.index)
+        dss = ds.score(test, test_csv['TARGET'])
+        print(
+            "The Adaboost method using decision stumps scores a precision of {} for n_estimators={}".format(dss, n))
+        dts = dt.score(test, test_csv['TARGET'])
+        print(
+            "The Adaboost method using decision trees scores a precision of {} for n_estimators={}".format(dts, n))
